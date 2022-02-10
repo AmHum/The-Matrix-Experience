3edf3b2ac75bc71f87bf8e8b6f4a7686af2bfb1c
Bubba <bubba.dave.7@gmail.com>
2022-02-09 18:23:10 -0700

    models created and starting seed files

--- models/index.js
+++ models/index.js
@@ -1,3 +1,33 @@
 const User = require('./User');
+const dashboard = require('./dashboard');
+const craft = require('./craft');
+const image = require("./image")
 
-module.exports = User;
+User.hasMany(dashboard, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+
+dashboard.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+dashboard.belongsTo(craft, {
+    foreignKey: 'craft_id'
+});
+
+craft.hasMany(dashboard, {
+    foreignKey: 'craft_id',
+    onDelete: 'CASCADE'
+});
+
+craft.hasMany(Image, {
+    foreignKey: 'craft_id',
+    onDelete: 'CASCADE'
+});
+
+Image.belongsTo(craft, {
+    foreignKey: 'craft_id'
+});
+
+module.exports = { User, dashboard, craft, image };
