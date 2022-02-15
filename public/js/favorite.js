8f091c34af1a8355f636f7f5d670c8bc88efb032
Wenwen Tian <wwtian9@gmail.com>
2022-02-15 14:18:38 -0700

    fixed bugs

--- public/js/favorite.js
+++ public/js/favorite.js
@@ -1,14 +1,14 @@
 async function favoriteClickHandler(event) {
   event.preventDefault();
 
-  const id = window.location.toString().split("/")[
+  const craft_id = window.location.toString().split("/")[
     window.location.toString().split("/").length - 1
   ];
 
-  const response = await fetch("/api/favorite", {
+  const response = await fetch(`/api/favorite`, {
     method: "POST",
     body: JSON.stringify({
-      craft_id: id,
+      craft_id: craft_id,
     }),
     headers: {
       "Content-Type": "application/json",
